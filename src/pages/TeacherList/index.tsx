dee114c1c185ceeec5786466fe257f135da175ec
Humberto Filho <humbertofilho5@gmail.com>
2020-08-09 11:27:02 -0400

    importando css

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import PageHeader from '../../components/PageHeader';
 import './styles.css';
+import TeacherItem from '../../components/TeacherItem';
 
 function TeacherList() {
   return (
@@ -32,32 +32,10 @@ function TeacherList() {
         </form>
       </PageHeader>
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars0.githubusercontent.com/u/56412569?s=460&u=b2b92410dbe3f9e77c19fe91efcfb83eefd11982&v=4"
-              alt="Humberto" />
-            <div>
-              <strong>Humberto Filho</strong>
-              <span>Química</span>
-            </div>
-          </header>
-          <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br />  <br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
-          </p>
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 80,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="whatsapp" />
-              Entrar em contato
-            </button>
-          </footer>
-
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   );
